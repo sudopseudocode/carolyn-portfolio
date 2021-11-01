e6d63271bf03e215bfb452a43b61c45fe8fb13f6
Paul DiLoreto <pdiloreto@twitter.com>
2021-11-01 16:36:34 -0700

    Added new hover state for project thumbnails

--- src/components/Projects/ProjectThumbnail.tsx
+++ src/components/Projects/ProjectThumbnail.tsx
@@ -1,20 +1,65 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import Img from 'gatsby-image';
 import { Link } from 'gatsby';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
 import { makeStyles } from '@material-ui/styles';
-import Zoom from '@material-ui/core/Zoom';
 import slugify from 'slugify';
 import { Project } from '../../types';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   photoContainer: {
     position: 'relative',
   },
   photo: {
     cursor: 'pointer',
   },
-});
+  labelContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+    opacity: 0,
+    transition: 'opacity 300ms ease-in-out',
+    '&:hover': {
+      transition: 'opacity 300ms ease-in-out',
+      opacity: 1,
+    },
+
+    // So that text and background have different opacities
+    // Don't want to use rgba() so that we can programmatically get the color from theme
+    '&::before': {
+      content: '""',
+      backgroundColor: theme.palette.background.default,
+      opacity: 0.9,
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      zIndex: 2,
+    },
+    '& h3,h4': {
+      margin: 0,
+      zIndex: 3,
+      color: theme.palette.primary.main,
+    },
+    '& h3': {
+      fontSize: '1.5em',
+      lineHeight: '1em',
+      marginBottom: '0.3em',
+    },
+    '& h4': {
+      fontSize: '1em',
+      fontWeight: 'bolder',
+    },
+  },
+}));
 
 interface ThumbnailProps {
   data: Project;
@@ -28,15 +73,16 @@ const ProjectThumbnail = (props: ThumbnailProps): ReactElement => {
     remove: /[^\w\s]/g,
     lower: true,
   });
-  const [labelActive, setLabel] = useState(false);
 
   return (
-    <div className={classes.photoContainer} onMouseEnter={() => setLabel(true)} onMouseLeave={() => setLabel(false)}>
+    <div className={classes.photoContainer}>
+      <div className={classes.labelContainer}>
+        <h3>{data.title}</h3>
+
+        <h4>{data.summary.summary}</h4>
+      </div>
       <Link to={`/projects/${slug}`}>
         <Img fluid={data.coverImage.fluid} alt={data.coverImage.title} className={classes.photo} />
-        <Zoom in={labelActive}>
-          <GridListTileBar title={data.title} subtitle={data.summary && data.summary.summary} className={classes.label} />
-        </Zoom>
       </Link>
     </div>
   );
