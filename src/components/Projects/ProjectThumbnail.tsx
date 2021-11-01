5630946bf3d6abf45045e0201bbf1f662a32fb5e
Paul DiLoreto <pdiloreto@twitter.com>
2021-10-31 23:19:50 -0700

    Removed hovered state on mobile

--- src/components/Projects/ProjectThumbnail.tsx
+++ src/components/Projects/ProjectThumbnail.tsx
@@ -4,7 +4,6 @@ import { Link } from 'gatsby';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import { makeStyles } from '@material-ui/styles';
 import Zoom from '@material-ui/core/Zoom';
-import detectIt from 'detect-it';
 import slugify from 'slugify';
 import { Project } from '../../types';
 
@@ -29,13 +28,13 @@ const ProjectThumbnail = (props: ThumbnailProps): ReactElement => {
     remove: /[^\w\s]/g,
     lower: true,
   });
-  const [labelActive, setLabel] = useState(detectIt.deviceType === 'touchOnly');
+  const [labelActive, setLabel] = useState(false);
 
   return (
     <div className={classes.photoContainer} onMouseEnter={() => setLabel(true)} onMouseLeave={() => setLabel(false)}>
       <Link to={`/projects/${slug}`}>
         <Img fluid={data.coverImage.fluid} alt={data.coverImage.title} className={classes.photo} />
-        <Zoom in={detectIt.deviceType === 'touchOnly' || labelActive}>
+        <Zoom in={labelActive}>
           <GridListTileBar title={data.title} subtitle={data.summary && data.summary.summary} className={classes.label} />
         </Zoom>
       </Link>
