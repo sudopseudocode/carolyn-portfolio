b99ee37d0d1a88bcd40b2478adcaa7fa617bf0bf
Paul DiLoreto <paul.diloreto@gmail.com>
2022-11-07 00:55:41 -0800

    chore(): remove Next, trying Sveltekit instead

--- utils/types/common.ts
+++
@@ -1,9 +0,0 @@
-export type Image = {
-  id: string;
-  url: string;
-  title: string;
-  description: string;
-  width: number;
-  height: number;
-  blurDataUrl: string;
-};
--- utils/types/pages.ts
+++
@@ -1,3 +0,0 @@
-export type PageProps = {};
-
-export type HomeData = {};
