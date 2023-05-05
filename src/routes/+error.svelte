44cf6e626b1240932e156db32b95c8006bf2d9ac
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-05 11:40:51 -0700

    feat(): added 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<div class="container">
+	<h1>404</h1>
+	<h2>Page not found</h2>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding-top: 2rem;
+	}
+	h1,
+	h2 {
+		margin: 0;
+	}
+</style>
