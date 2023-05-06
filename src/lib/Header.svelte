735236e69305026f7798cc179a1ba2ab3a58ee42
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-05 22:24:02 -0700

    feat(): finished Header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -6,6 +6,7 @@
 		{ name: 'Photography', path: '/photography' },
 		{ name: 'Resume', path: '/resume' }
 	];
+	let mobileNavOpen = false;
 </script>
 
 <header class={$page.url.pathname === '/' ? 'home' : ''}>
@@ -13,22 +14,32 @@
 		<svg inline-src="logo" />
 		<h1>Carolyn DiLoreto</h1>
 	</a>
-	<nav>
+	<nav class={`mobile-nav ${mobileNavOpen ? 'mobile-open' : ''}`}>
 		{#each links as link}
-			<a href={link.path} class={$page.url.pathname === link.path ? 'active' : ''}>
+			<a
+				href={link.path}
+				class={$page.url.pathname === link.path ? 'active' : ''}
+				on:click={() => mobileNavOpen = false}
+			>
 				{`${link.name}.`}
 			</a>
 		{/each}
 	</nav>
+	<button class="mobile-nav-button" on:click={() => mobileNavOpen = !mobileNavOpen}>
+		<div class={`hamburger-icon ${mobileNavOpen ? 'close-icon' : ''}`} />
+	</button>
 </header>
 
 <style>
+	:root {
+		--mobile-nav-size: 2.5rem;
+	}
 	header {
 		display: flex;
-		justify-content: space-between;
 		background-color: var(--dark-color);
 		padding: 0rem var(--padding);
 		align-items: center;
+		z-index: 1;
 	}
 	a,
 	a:visited {
@@ -46,8 +57,9 @@
 		display: flex;
 		align-items: flex-end;
 		margin: 0.5rem 0;
+		flex-grow: 1;
 	}
-	svg {
+	.brand svg {
 		height: 3rem;
 	}
 	h1 {
@@ -59,19 +71,112 @@
 		line-height: 1.75rem;
 	}
 
+	.mobile-nav-button {
+		display: none;
+	}
 	nav a {
 		margin-left: 2rem;
 	}
 	nav a.active {
 		color: var(--light-color);
 	}
-	@media (max-width: 600px) {
-		header {
-			flex-direction: column;
-			align-items: flex-start;
+	@media (max-width: 414px) {
+		:root {
+			--mobile-nav-size: 2rem;
 		}
+		.brand {
+			display: flex;
+			justify-content: center;
+		}
+		.brand svg {
+			height: 2rem;
+		}
+		.brand h1 {
+			font-size: 1.5rem;
+			line-height: 1.5rem;
+			margin-left: 0;
+		}
+	}
+	@media (max-width: 820px) {
 		nav {
-			margin-top: 1rem;
+			display: none;
+		}
+		.mobile-nav-button {
+			display: block;
+			width: var(--mobile-nav-size);
+			height: var(--mobile-nav-size);
+			border: 1px solid var(--light-color);
+			border-radius: .2rem;
+			background-color: transparent;
+			cursor: pointer;
+			z-index: 1;
+		}
+		.hamburger-icon {
+			position: relative;
+			flex: none;
+			width: 100%;
+			height: 2px;
+			background-color: var(--light-text);
+			transition: all 250ms ease-in-out;
+		}
+		.hamburger-icon:before,
+		.hamburger-icon:after {
+			content: '';
+			position: absolute;
+			top: calc(var(--mobile-nav-size) / -6);
+			left: 0;
+			width: 100%;
+			height: 2px;
+			background-color: var(--light-text);
+			transition: var(--menu-transition);
+		}
+		.hamburger-icon:after {
+			top: calc(var(--mobile-nav-size) / 6);
+		}
+
+		.close-icon {
+			transform: rotate(135deg);
+		}
+		.close-icon:before,
+		.close-icon:after {
+			top: 0;
+			transform: rotate(90deg);
+		}
+		.close-icon:after {
+			opacity: 0;
+		}
+
+		.mobile-nav {
+			display: flex;
+			position: fixed;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			opacity: 0;
+			transform: scale(0);
+			transition: all 250ms ease-in-out;
+		}
+		.mobile-open {
+			opacity: 1;
+			transform: scale(1);
+			transition: all 250ms ease-in-out;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100vh;
+			background-color: var(--dark-color);
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+		}
+		.mobile-nav a {
+			display: flex;
+			justify-content: center;
+			width: 100%;
+			padding: 3rem;
 		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,78 @@
 </script>
 
 <Background image={data.backgroundImage} />
+<div class="container">
+	<div class="brand">
+		<svg inline-src="logo" />
+		<h1>Carolyn DiLoreto</h1>
+	</div>
+	<div class="links">
+		<a href="/photography">View Photography</a>
+		<a href="/projects">View Projects</a>
+	</div>
+</div>
+
+<style>
+	.container {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+	.brand {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.brand svg {
+		height: 8rem;
+		margin-bottom: 2rem;
+	}
+	.brand h1 {
+		font-size: 3.5rem;
+		color: var(--light-text);
+		margin: 0;
+	}
+	.links {
+		display: flex;
+		justify-content: space-between;
+		margin-top: 2rem;
+		width: 28rem;
+	}
+	.links a {
+		display: flex;
+		justify-content: center;
+		font-size: 1rem;
+		color: var(--light-color);
+		text-decoration: none;
+		border: 1px solid var(--light-color);
+		padding: .5rem;
+		width: 10rem;
+		transition: background-color .2s ease-in-out;
+	}
+	.links a:hover {
+		background-color: var(--light-color);
+		color: black;
+		transition: background-color .2s ease-in-out;
+	}
+	@media (max-width: 820px) {
+		.brand svg {
+			height: 6rem;
+		}
+		.brand h1 {
+			font-size: 2rem;
+		}
+		.links {
+			flex-direction: column;
+			align-items: center;
+		}
+		.links a {
+			margin-bottom: 1rem;
+		}
+	}
+</style>
