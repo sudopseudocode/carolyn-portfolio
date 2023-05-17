301d7594a7c0f2cbdf919ac1e832bf1d2598050c
Paul DiLoreto <paul.diloreto@gmail.com>
2023-05-16 17:51:59 -0700

    chore(): projects page wip

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -6,6 +6,10 @@
 </script>
 
 <div class="container">
+	<a href="/projects" class="go-back">
+		<svg inline-src="left-arrow" />
+		Go Back
+	</a>
 	<section class="header">
 		<div class="info">
 			<h1>{data.project.title}</h1>
@@ -22,16 +26,55 @@
 	<div id="markdown-container">
 		<Markdown source={data.project.description} />
 	</div>
+	<a href="/projects" class="view-more"> View More Work </a>
 </div>
 
 <style>
 	.container {
+		padding: 2rem var(--padding);
+		max-width: 1000px;
+		margin: auto;
+	}
+	.go-back {
+		transition: all 250ms ease-in-out;
+		background-color: transparent;
+		border: 1px solid var(--dark-color);
+		color: var(--dark-color);
+		display: inline-flex;
+		align-items: center;
+		text-decoration: none;
+		padding: 0.5rem 1rem;
+		font-size: 0.9rem;
+		margin-bottom: 2rem;
+	}
+	.go-back:hover {
+		background-color: var(--dark-transparent-color);
+		border: 1px solid var(--dark-transparent-color);
+		transition: all 250ms ease-in-out;
+	}
+	.go-back svg {
+		width: 1.5rem;
+		fill: var(--dark-color);
+	}
+	.view-more {
+		background-color: var(--dark-color);
+		color: var(--light-color);
+		text-decoration: none;
+		margin: auto;
+		display: inline;
+		padding: 0.5rem 1rem;
+		font-size: 0.9rem;
 	}
 	.header {
-		display: flex;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+	.image {
+		width: 100%;
 	}
 	.info h1 {
-		font-size: 3rem;
+		font-size: 3.8rem;
+		margin-bottom: 0.5rem;
 	}
 	.info h2 {
 		font-family: var(--body-font);
