eacf04d821211fed981e2f3cc54aef9aaac72716
Paul DiLoreto <paul.diloreto@gmail.com>
2023-06-05 23:49:26 -0700

    fix(): remove sharp

--- src/lib/utils/images.ts
+++ src/lib/utils/images.ts
@@ -1,13 +1,14 @@
 import { encode } from 'blurhash';
-import sharp from 'sharp';
+// import sharp from 'sharp';
 
 async function getImageData(imageUrl: string) {
-	const response = await fetch(imageUrl);
-	const buffer = await response.arrayBuffer();
-	return sharp(buffer).raw().ensureAlpha().toBuffer({ resolveWithObject: true });
+	// const response = await fetch(imageUrl);
+	// const buffer = await response.arrayBuffer();
+	// return sharp(buffer).raw().ensureAlpha().toBuffer({ resolveWithObject: true });
 }
 
 export async function encodeImageToBlurhash(url: string) {
 	const image = await getImageData(url);
-	return encode(image.data, image.info.width, image.info.height, 4, 4);
+	// return encode(image.data, image.info.width, image.info.height, 4, 4);
+	return '';
 }
